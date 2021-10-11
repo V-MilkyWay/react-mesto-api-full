ae89d78380c8a3eacf28f934a8ff7015c89f2d2f
Vladislav Milushkov <radshura@yandex.ru>
2021-10-11 22:51:04 +0300

    full

--- backend/controllers/users.js
+++ backend/controllers/users.js
@@ -189,7 +189,7 @@ module.exports.login = (req, res, next) => {
       const token = jwt.sign({ _id: user._id }, NODE_ENV === 'production' ? JWT_SECRET : 'super-strong-secret', { expiresIn: '7d' });
       res.cookie('jwt', token, {
         maxAge: 3600000 * 24 * 7,
-        httpOnly: true,
+        httpOnly: false,
         sameSite: true,
       })
         .status(200)
