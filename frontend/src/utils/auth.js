10440c7277fa9393e8d103e1b6cbb359fbd58995
Vladislav Milushkov <radshura@yandex.ru>
2021-10-11 22:20:54 +0300

    full

--- frontend/src/utils/auth.js
+++ frontend/src/utils/auth.js
@@ -34,6 +34,7 @@ export const authorize = (email, password) => {
   })
   .then((response => response.json()))
     .then((data) => {
+      console.log(data)
       if (data.token) {
         localStorage.setItem('jwt', data.token);
         return data;
